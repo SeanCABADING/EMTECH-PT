22c86acb6289cfa0258d5965572028c244e2904f
SeaCru <sean.cruz150251@my.lsgh.edu.ph>
2021-05-02 15:32:24 +0800

    2 files modified

--- pages/contact/contact.ts
+++ pages/contact/contact.ts
@@ -6,9 +6,37 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+ Sex: string;
+ Height: number;
+ Weight: number;
+ Age: number;
+ Calories: number;
+ total: number;
+ activity: number;
+ CalorieMessage: string;
+ Note: string;
+ x: string = "testing";
+ y: string = "test also";
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {}
 
-  }
+   calculatedCalories(){
+
+     if (this.Sex === "m") { 
+      this.Calories = (10 * this.Weight + 6.25 * this.Height - 5 * this.Age + 5);
+     }
+
+     else if  (this.Sex === "f") { 
+      this.Calories = (10 * this.Weight + 6.25 * this.Height - 5 * this.Age - 161)
+     }
+
+     else{
+      this.Calories = 0.01;
+     }
+     this.total = parseFloat((this.Calories * this.activity).toFixed(2));
+   }
 
 }
+     
+
+
